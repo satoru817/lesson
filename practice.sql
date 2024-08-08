b3da8537db371667b582ff717f8ec5dee2e2ec06
satoru_kurita <rhodeus208@gmail.com>
2024-08-09 07:36:41 +0900

    revised_sql

--- practice.sql
+++ practice.sql
@@ -17,8 +17,9 @@ SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10001 A
 SELECT emp_no , dept_no ,from_date,to_date from dept_emp WHERE dept_no IN ('d004','d008') AND((from_date<='1995_01_01' AND to_date >= '1995_01_01')OR(from_date<='1997_01_01' AND to_date >= '1997_01_01'));
 /*&&と||の書き直しをしました、IN句を使いました*/
 
-select max(salary), first_name,last_name from dept_emp inner join departments on dept_emp.dept_no = departments.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no inner join employees on employees.emp_no = dept_emp.emp_no where dept_name = 'Customer Service';
+select max(salary) as MaxSalary, first_name,last_name from dept_emp de inner join departments dp on dp.dept_no = de.dept_no inner join salaries sl on sl.emp_no = de.emp_no inner join employees ep on ep.emp_no = de.emp_no where dept_name = 'Customer Service' group by dept_name;
 /*dept_nameの指定を追加しました*/
+/*エイリアスを利用しました。group by dept_nameを追加しました。*/
 
 /*部署ごとの現在の給料の平均値*/
 select avg(salary) as avs ,dept_name from dept_emp inner join departments on departments.dept_no = dept_emp.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no where salaries.to_date >= CURDATE() AND dept_emp.to_date >= CURDATE() group by dept_name;
@@ -27,5 +28,5 @@ select avg(salary) as avs ,dept_name from dept_emp inner join departments on dep
 select first_name ,last_name from employees where emp_no not in (select emp_no from dept_manager);
 
 /*部署ごとに現在給料が最も高い人を抽出*/
-SELECT dept_name,salary,first_name,last_name FROM departments JOIN (select dept_no,max(salary) as MaxSalary from dept_emp JOIN salaries on dept_emp.emp_no = salaries.emp_no where dept_emp.to_date >= CURDATE() AND salaries.to_date >= CURDATE() group by dept_no) AS deptMaxSalary on departments.dept_no = deptMaxSalary.dept_no JOIN salaries on salaries.salary = deptMaxSalary.MaxSalary JOIN employees ON employees.emp_no = salaries.emp_no;
+SELECT dept_name,salary,first_name,last_name FROM departments INNER JOIN (select dept_no,max(salary) as MaxSalary from dept_emp INNER  JOIN salaries on dept_emp.emp_no = salaries.emp_no where dept_emp.to_date >= CURDATE() AND salaries.to_date >= CURDATE() group by dept_no) AS deptMaxSalary on departments.dept_no = deptMaxSalary.dept_no INNER JOIN salaries on salaries.salary = deptMaxSalary.MaxSalary INNER JOIN employees ON employees.emp_no = salaries.emp_no;
 /*サブクエリを利用して書き直しました。*/
