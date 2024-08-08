ef8b7dba97f3610fdd10a1848a81ad347981a23f
satoru_kurita <rhodeus208@gmail.com>
2024-08-08 10:16:18 +0900

    reviesed_sql

--- practice.sql
+++ practice.sql
@@ -1,22 +1,31 @@
-SELECT * FROM employees WHERE employees.birth_date LIKE '1965_01_%' ;
+SELECT * FROM employees WHERE employees.birth_date BETWEEN '1965-01-01' AND '1965-01-31';
+/*betweenを利用しました*/
 
-SELECT COUNT(gender) FROM employees WHERE gender = 'F' ;
+select gender ,count(gender) from employees group by gender;
+/*group byを利用して書き直しました。*/
 
-SELECT COUNT(gender) FROM employees WHERE gender = 'M' ;
 
-SELECT dept_name, COUNT(*) FROM dept_emp INNER JOIN employees ON dept_emp.emp_no = employees.emp_no INNER JOIN departments ON dept_emp.dept_no = departments.dept_no GROUP BY dept_name ; 
+SELECT dept_name, COUNT(*) FROM dept_emp INNER JOIN departments ON dept_emp.dept_no = departments.dept_no GROUP BY dept_name ; 
+/*無駄なjoinをなくしました*/
 
-SELECT  dept_name,first_name , last_name FROM (dept_manager INNER JOIN employees ON dept_manager.emp_no = employees.emp_no INNER JOIN departments ON departments.dept_no = dept_manager.dept_no) WHERE to_date LIKE '9%' ;
+SELECT  dept_name,first_name , last_name FROM dept_manager INNER JOIN employees ON dept_manager.emp_no = employees.emp_no INNER JOIN departments ON departments.dept_no = dept_manager.dept_no WHERE to_date >= CURDATE() ;
+/*CURDATE()に変更しました。*/
 
-SELECT emp_no, first_name, last_name FROM employees WHERE 10001 <= emp_no && emp_no <= 10100 && first_name LIKE 'P%';
+SELECT emp_no, first_name, last_name FROM employees WHERE emp_no BETWEEN 10001 AND 10100 AND first_name LIKE 'P%';
+/*ANDにへんこうしました,BETWEENを利用しました*/
 
-SELECT emp_no , dept_no ,from_date,to_date from dept_emp WHERE (dept_no = 'd004'|| dept_no = 'd008') &&((from_date<='1995_01_01' && to_date >= '1995_01_01')||(from_date<='1997_01_01' && to_date >= '1997_01_01'));
+SELECT emp_no , dept_no ,from_date,to_date from dept_emp WHERE dept_no IN ('d004','d008') AND((from_date<='1995_01_01' AND to_date >= '1995_01_01')OR(from_date<='1997_01_01' AND to_date >= '1997_01_01'));
+/*&&と||の書き直しをしました、IN句を使いました*/
 
-select max(salary), first_name,last_name from dept_emp inner join departments on dept_emp.dept_no = departments.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no inner join employees on employees.emp_no = dept_emp.emp_no;
+select max(salary), first_name,last_name from dept_emp inner join departments on dept_emp.dept_no = departments.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no inner join employees on employees.emp_no = dept_emp.emp_no where dept_name = 'Customer Service';
+/*dept_nameの指定を追加しました*/
 
-select avg(salary) as avs ,dept_name from dept_emp inner join departments on departments.dept_no = dept_emp.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no where salaries.to_date LIKE '9%' group by dept_name;
+/*部署ごとの現在の給料の平均値*/
+select avg(salary) as avs ,dept_name from dept_emp inner join departments on departments.dept_no = dept_emp.dept_no inner join salaries on salaries.emp_no = dept_emp.emp_no where salaries.to_date >= CURDATE() AND dept_emp.to_date >= CURDATE() group by dept_name;
+/*dept_emp.to_data>=CURDATE()の条件を追加しました*/
 
 select first_name ,last_name from employees where emp_no not in (select emp_no from dept_manager);
 
-select dept_name, max(salary) as ms,first_name, last_name from employees inner join salaries on salaries.emp_no = employees.emp_no inner join dept_emp on dept_emp.emp_no = employees.emp_no inner join departments on departments.dept_no = dept_emp.dept_no where salaries.to_date Like '9%' group by dept_name;
-
+/*部署ごとに現在給料が最も高い人を抽出*/
+SELECT dept_name,salary,first_name,last_name FROM departments JOIN (select dept_no,max(salary) as MaxSalary from dept_emp JOIN salaries on dept_emp.emp_no = salaries.emp_no where dept_emp.to_date >= CURDATE() AND salaries.to_date >= CURDATE() group by dept_no) AS deptMaxSalary on departments.dept_no = deptMaxSalary.dept_no JOIN salaries on salaries.salary = deptMaxSalary.MaxSalary JOIN employees ON employees.emp_no = salaries.emp_no;
+/*サブクエリを利用して書き直しました。*/
